dd3f9c5dbefa257a0a84871387affd7c79f6ea03
zyouime <igorneizvestny69@gmail.com>
2025-03-15 03:25:26 +0200

    cfg fix

--- src/main/java/me/zyouime/indicator/setting/BooleanSetting.java
+++ src/main/java/me/zyouime/indicator/setting/BooleanSetting.java
@@ -1,12 +1,9 @@
 package me.zyouime.indicator.setting;
 
-import me.zyouime.indicator.config.ConfigKey;
-import net.minecraft.client.util.math.MatrixStack;
-
 public class BooleanSetting extends Setting<Boolean> {
 
-    public BooleanSetting(Boolean value, ConfigKey configKey) {
-        super(value, configKey);
+    public BooleanSetting(String configKey) {
+        super(configKey, Boolean.class);
     }
 
 }
--- src/main/java/me/zyouime/indicator/setting/NumberSetting.java
+++ src/main/java/me/zyouime/indicator/setting/NumberSetting.java
@@ -1,12 +1,9 @@
 package me.zyouime.indicator.setting;
 
-import me.zyouime.indicator.config.ConfigKey;
-import net.minecraft.client.util.math.MatrixStack;
-
 public class NumberSetting extends Setting<Float> {
 
-    public NumberSetting(Float value, ConfigKey configKey) {
-        super(value, configKey);
+    public NumberSetting(String configKey) {
+        super(configKey, Float.class);
     }
 
 }
