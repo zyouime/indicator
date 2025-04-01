baa9678482d8737a579592d74a8f10e8ff21a064
zyouime <igorneizvestny69@gmail.com>
2025-04-01 23:15:43 +0300

    :(

--- src/main/java/me/zyouime/indicator/config/ConfigData.java
+++ src/main/java/me/zyouime/indicator/config/ConfigData.java
@@ -6,29 +6,15 @@ import java.lang.reflect.Field;
 
 public class ConfigData {
 
-    @Expose
     boolean enabled = true;
-    @Expose
     boolean animation = true;
-    @Expose
     boolean inOneRow = false;
-    @Expose
     float heartsSize = 25f;
-    @Expose
     float heartsOffset = 0.5f;
-    @Expose
     boolean showOnMobs = true;
-    @Expose
     boolean showOnPlayers = true;
-    @Expose
-    boolean showHealthInInvisibleEntities = true;
-    @Expose
     boolean showYourHealth = false;
-    @Expose
-    boolean showDisplayNameForInvisibleEntities = true;
-    @Expose
     boolean showYourDisplayName = false;
-    @Expose
     boolean scoreboardMode = false;
 
     public void setField(String fieldName, Object value) {
